201ca3bf90cc5325ea033373ff3661fc856dc148
Volodymyr Seliverstov <VolodymyrSeliverstov@smartexe.com>
2017-12-05 12:28:08 +0200

    Initial commit

---
+++ JenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JenkinsTest
+//
+//  Created by Volodymyr Seliverstov on 12/5/17.
+//  Copyright © 2017 SkySoftTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JenkinsTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JenkinsTest
+//
+//  Created by Volodymyr Seliverstov on 12/5/17.
+//  Copyright © 2017 SkySoftTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
